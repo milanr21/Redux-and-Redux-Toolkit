12d90aa98031bc0bfe320205d59e1ec719ff7993
milanr22 <milanrawal@fintech.com.np>
2023-08-19 21:18:52 +0545

    redux-chapter-1 ayncFun

--- index.js
+++ index.js
@@ -1,26 +1,62 @@
 import { createStore, applyMiddleware } from 'redux';
+import axios from 'axios';
 
 import logger from 'redux-logger';
 
+const init = 'init';
+const increament = 'increment';
+const decrement = 'decrement';
+const incrementByAmount = 'incrementbyAmount';
+
 const store = createStore(reducer, applyMiddleware(logger.default));
 
-function reducer(state = { amount: 1 }, action) {
-  if (action.type === 'increment') {
-    return { amount: state.amount + 1 };
-  }
+let history = [];
 
-  return state;
+// action name constants
+
+function reducer(state = { amount: 1 }, action) {
+  //immutability --- increment
+
+  switch (action.type) {
+    case init:
+      return { amount: action.payload };
+
+    case increament:
+      return { amount: state.amount + 1 };
+    case decrement:
+      return { amount: state.amount - 1 };
+    case incrementByAmount:
+      return { amount: state.amount + action.payload };
+    default:
+      return state;
+  }
 }
 
 // console.log(store.getState());
-
-let history = [];
 
 store.subscribe(() => {
   history.push(store.getState());
   console.log(history);
 });
 
+// Action Creators
+
+function initUsers(value) {
+  return { type: init, payload: value };
+}
+
+function decrementFuc() {
+  return { type: decrement };
+}
+
+function incrementByAmountFuc(value) {
+  return { type: incrementByAmount, payload: value };
+}
+
+// setInterval(() => {
+//   store.dispatch(incrementByAmountFuc(3));
+// }, 2000);
+
 setInterval(() => {
-  store.dispatch({ type: 'increment' });
-}, 4000);
+  store.dispatch(initUsers(200));
+}, 2000);
